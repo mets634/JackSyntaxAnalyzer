f4438da146874fe6f247d45db92bc99ee85adc5d
yonah mann <mets634@gmail.com>
2017-06-04 18:40:25 +0300

    Initial commit

---
+++ JackSyntaxAnalyzer/LexicalElements.fs
@@ -0,0 +1,70 @@
+﻿module LexicalElements
+
+open System
+
+
+(*In this module, each type of element
+is given a predicate to determine if a 
+given string a of that element. The predicate
+is in the following format: isPredicateName*)
+
+
+/// KEYWORD ///
+
+type keyWordType =
+                | CLASS
+                | CONSTRUCTOR
+                | FUNCTION
+                | METHOD
+                | FIELD
+                | STATIC
+                | VAR
+                | INT
+                | CHAR
+                | BOOLEAN
+                | VOID
+                | TRUE
+                | FALSE
+                | NULL
+                | THIS
+                | LET
+                | DO
+                | IF
+                | ELSE
+                | WHILE
+                | RETURN
+
+
+let keyWordList = ["class"; "constructor"; "function";
+                    "method"; "field"; "static"; "var";
+                    "int"; "char"; "boolean"; "void"; "true";
+                    "false"; "null"; "this"; "let";
+                    "do"; "if"; "else"; "while"; "return"]
+
+let isKeyWord (str:string) = 
+    List.contains str keyWordList
+
+
+/// SYMBOL ///
+
+let symbolList = ["{"; "}"; "("; ")"; "["; "]";
+                   ","; "."; ";"; "+"; "-"; "*"; @"/";
+                   "&"; "|"; "<"; ">"; "="; "~"]
+
+let isSymbol (str:string) = 
+    List.contains str symbolList
+
+
+/// INTEGER CONSTANT ///
+
+let isIntegerConstant (str:string) =
+    try
+        let number = Int32.Parse str
+        number >= 0 && number < 32767
+
+    with _ -> false  // not a valid integer
+
+
+/// STRING CONSTANT ///
+
+let 
---
+++ JackSyntaxAnalyzer/Tokenizer.fs
@@ -0,0 +1,31 @@
+﻿module Tokenizer
+
+open System
+open System.IO
+open LexicalElements
+
+(*This module represents Jack's 
+tokenizer. The tokenizer is a FSM 
+who is described in the word 
+document of this project.*)
+
+type elementType = KeyWord | Symbol | IntegerConstant | StringConstant | Identifier
+type tokenRecord = { eType:elementType; value:string }
+
+
+let Q6 (str:string) = { eType=elementType.StringConstant; value=str }
+
+let Q5 (character:Char) = 
+
+let Q4 (sym:Char) = { eType=elementType.Symbol; value=sym }
+
+//let Q3 (character:Char) = 
+//
+//let Q2 (character:Char) = 
+//
+//let Q1 (word:string) = 
+
+let Q0 (f:StreamReader) = 
+    let curr = ""
+    match f.Read() with
+    | 
